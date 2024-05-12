2a6acf2484249d57e8935b580e374e9aca289715
ccrawford4 <calumcrawford9@gmail.com>
2024-05-12 06:56:15 -0700

    Initial Commit

---
+++ Hello_World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello_World
+//
+//  Created by Calum Crawford on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
