0253daf946a04d697ff112d9b85b4357df0701b4
ccrawford4 <calumcrawford9@gmail.com>
2024-05-12 09:01:54 -0700

    Added list support

---
+++ Hello_World/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  Hello_World
+//
+//  Created by Calum Crawford on 5/12/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) {
+            scrum in
+            CardView(scrum: scrum)
+            // Should be different colors
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
