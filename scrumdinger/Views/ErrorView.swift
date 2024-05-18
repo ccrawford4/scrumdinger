43c24367ea960106ae47328745011c2422111c01
ccrawford4 <calumcrawford9@gmail.com>
2024-05-18 09:31:04 -0700

    Added ErrorWrapper

---
+++ scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,42 @@
+//
+//  ErrorView.swift
+//  scrumdinger
+//
+//  Created by Calum Crawford on 5/18/24.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occured!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
